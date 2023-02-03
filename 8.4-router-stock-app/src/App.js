31b655f4ed69fb51af99ffd1d9da85a5a7c4d2e5
Tijuana22 <tijuanahunt39@gmail.com>
2023-02-03 14:25:51 -0500

    first commit

---
+++ 8.4-router-stock-app/src/App.js
@@ -0,0 +1,17 @@
+import About from "./components/About";
+import Home from "./components/Home";
+
+// import data from "./data.js"
+
+
+function App() {
+  return (
+    <div className="App">
+    
+    <Home/>
+    <About/>
+    </div>
+  );
+}
+
+export default App;
